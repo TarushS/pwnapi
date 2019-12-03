cde92101862a0ff8c8627b9abdf47a68dba30574
nico <nico>
2019-12-03 19:12:39 +0100

    replaced em dash character with unicode escape seq

--- pwnlib/logger.py
+++ pwnlib/logger.py
@@ -13,18 +13,18 @@ class logger(object):
 	def error(self, msg, details=None):
 		if self.level >= 0:
 			if details is not None:
-				msg += "\n{}\n{}\n{}".format("─"*term.columns, details, "─"*term.columns)
+				msg += "\n{}\n{}\n{}".format("\u2014"*term.columns, details, "─"*term.columns)
 			self.stderr.write("[{}]: {}\n".format(red("ERROR"), msg))
 	def info(self, msg, details=None):
 		if self.level >= 1:
 			if details is not None:
-				msg += "\n{}\n{}\n{}".format("─"*term.columns, details, "─"*term.columns)
+				msg += "\n{}\n{}\n{}".format("\u2014"*term.columns, details, "─"*term.columns)
 			self.stdout.write("[{}]:  {}\n".format(green("INFO"), msg))
 
 	def debug(self, msg, details=None):
 		if self.level >= 2:
 			if details is not None:
-				msg += "\n{}\n{}\n{}".format("─"*term.columns, details, "─"*term.columns)
+				msg += "\n{}\n{}\n{}".format("\u2014"*term.columns, details, "─"*term.columns)
 			self.stderr.write("[{}]: {}\n".format(yellow("DEBUG"), msg))
 
 log = logger()
